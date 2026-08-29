6a66a62721ba71b35ea00fd07a9c163921497841
DAKSH GOEL <145486282+dakshgoel2008@users.noreply.github.com>
2026-08-29 22:18:08 +0530

    Implement Character-level encode/decode (Build GPT 2/10)

---
+++ data/vocab.py
@@ -0,0 +1,17 @@
+from typing import Dict, List, Tuple
+
+class Solution:
+    def build_vocab(self, text: str) -> Tuple[Dict[str, int], Dict[int, str]]:
+        a=sorted(set(text))
+        stoi={}
+        itos={}
+        for k,v in enumerate(a):
+            itos[k]=v
+            stoi[v]=k;
+        return (stoi,itos)
+
+    def encode(self, text: str, stoi: Dict[str, int]) -> List[int]:
+        return [stoi[c] for c in text]
+
+    def decode(self, ids: List[int], itos: Dict[int, str]) -> str:
+        return ''.join(itos[c] for c in ids)
